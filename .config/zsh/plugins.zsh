5883c9fa0ec3b2e23a90a3e6f4f2cfb781828f43
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-05-31 16:08:28 +0900

    Updates

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -5,7 +5,7 @@ plugins=(
     "Aloxaf/fzf-tab"
     "kazhala/dotbare"
     "kutsan/zsh-system-clipboard"
-    # "MichaelAquilina/zsh-you-should-use"
+    "MichaelAquilina/zsh-you-should-use"
     # "marlonrichert/zsh-autocomplete"
     "ohmyzsh/command-not-found"
     "ohmyzsh/sudo"
