5bad55b792dcb7f9738b9f02a85f6a0a2081f893
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-06-18 07:21:51 +0900

    Init

--- .config/zsh/scripts.zsh
+++ .config/zsh/scripts.zsh
@@ -20,6 +20,32 @@ insert-last-command-output() {
 
 ###########################################################################################
 ###########################################################################################
+### --- Weather --- ###
+weath() {
+    [ "$MANPAGER" = "less -s" ] && pager=true || pager=false
+    [ "$pager" = "false" ] || {
+        export MANPAGER='less -s'
+        export LESS="R"
+        export LESS_TERMCAP_mb="$(printf '%b' '[1;31m')"
+        export LESS_TERMCAP_md="$(printf '%b' '[1;36m')"
+        export LESS_TERMCAP_me="$(printf '%b' '[0m')"
+        export LESS_TERMCAP_so="$(printf '%b' '[01;44;33m')"
+        export LESS_TERMCAP_se="$(printf '%b' '[0m')"
+        export LESS_TERMCAP_us="$(printf '%b' '[1;32m')"
+        export LESS_TERMCAP_ue="$(printf '%b' '[0m')"
+        export LESSOPEN="| /usr/bin/highlight -O ansi %s 2>/dev/null"
+    }
+    less -S ${XDG_CACHE_HOME:-${HOME}/.cache}/weatherreport
+    [ "$pager" = "false" ] || {
+        export MANPAGER="sh -c 'col -bx | batman -l man -p'"
+        export MANROFFOPT="-c"
+        eval "$(batman --export-env)"
+    }
+}
+
+
+###########################################################################################
+###########################################################################################
 ### --- Mount ecryptfs --- ###
 emt() {
     echo "$(pass show encryption/ecryptfs)" | sudo mount -t ecryptfs "$1" "$2" \
